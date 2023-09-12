ea718ef4a3675534ae7a58f30a495dcb020b9008
awaisnazir08 <owaisnazir2228@gmail.com>
2023-09-12 17:45:05 +0500

    connected mongodb, mongoose and nodemon

---
+++ backend/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb://0.0.0.0/octa"
+
+const connectToMongo = async () => {
+    try {
+        mongoose.set("strictQuery", false);
+        mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // Other options if needed
+});
+        console.log("Connected to mongoDB successfully");
+    } catch (error) {
+        console.log(error);
+        process.exit();
+    }
+};
+module.exports = connectToMongo; 
