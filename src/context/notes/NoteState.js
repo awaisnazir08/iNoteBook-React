8ad0a70d2cee1919e2049a971ce15f27bcfbd1df
awaisnazir08 <owaisnazir2228@gmail.com>
2024-01-20 21:35:16 +0500

    made a separate noteitem component

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -37,6 +37,60 @@ const NoteState = (props) => {
             "tag": "Personal",
             "date": "2024-01-20T09:31:06.348Z",
             "__v": 0
+        },
+        {
+            "_id": "65ab78b5a49d49d6618bae1f",
+            "user": "65aab9928d78129bd5d0e98d",
+            "title": "My title for first note",
+            "description": "My first note's description is just lame fsd",
+            "tag": "Personal",
+            "date": "2024-01-20T07:39:33.268Z",
+            "__v": 0
+        },
+        {
+            "_id": "65ab815f5a774bb570064f8a",
+            "user": "65aab9928d78129bd5d0e98d",
+            "title": "My updated title for second note",
+            "description": "My updated second note's description is also lame..!!",
+            "tag": "Personal",
+            "date": "2024-01-20T08:16:31.124Z",
+            "__v": 0
+        },
+        {
+            "_id": "65ab92da3a206fe2612fc6d5",
+            "user": "65aab9928d78129bd5d0e98d",
+            "title": "My title for second note",
+            "description": "My second note's description is also lame..!!",
+            "tag": "Personal",
+            "date": "2024-01-20T09:31:06.348Z",
+            "__v": 0
+        },
+        {
+            "_id": "65ab78b5a49d49d6618bae1f",
+            "user": "65aab9928d78129bd5d0e98d",
+            "title": "My title for first note",
+            "description": "My first note's description is just lame fsd",
+            "tag": "Personal",
+            "date": "2024-01-20T07:39:33.268Z",
+            "__v": 0
+        },
+        {
+            "_id": "65ab815f5a774bb570064f8a",
+            "user": "65aab9928d78129bd5d0e98d",
+            "title": "My updated title for second note",
+            "description": "My updated second note's description is also lame..!!",
+            "tag": "Personal",
+            "date": "2024-01-20T08:16:31.124Z",
+            "__v": 0
+        },
+        {
+            "_id": "65ab92da3a206fe2612fc6d5",
+            "user": "65aab9928d78129bd5d0e98d",
+            "title": "My title for second note",
+            "description": "My second note's description is also lame..!!",
+            "tag": "Personal",
+            "date": "2024-01-20T09:31:06.348Z",
+            "__v": 0
         }
     ]
     const [notes, setNotes] = useState(notesInitial);
