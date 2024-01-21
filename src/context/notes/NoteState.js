014a965fda3d537862353a9e4663527198950086
awaisnazir08 <owaisnazir2228@gmail.com>
2024-01-22 00:43:55 +0500

    fixed a bug, now everything updates visible on the front end as soon as user updates a note, no page reload required

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -71,14 +71,14 @@ const NoteState = (props) => {
         let newNotes = JSON.parse(JSON.stringify(notes));
         //Logic to edit in client
         for (let index = 0; index < newNotes.length; index++) {
-            const element = newNotes[index];
-            if (element._id === id) {
-                element.title = title;
-                element.description = description;
-                element.tag = tag;
+            // const element = newNotes[index];
+            if (newNotes[index]._id === id) {
+                newNotes[index].title = title;
+                newNotes[index].description = description;
+                newNotes[index].tag = tag;
+                setNotes(newNotes);
                 break;
             }
-            setNotes(newNotes);
         }
     }
 
